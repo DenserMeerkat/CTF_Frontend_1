658c06f59569e108009aaa2fe29756f4102db529
Denser Meerkat <mragulmanoharan@gmail.com>
2022-10-16 05:41:57 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function menu_toggle(){ 
+    menu = document.getElementById("navig");
+    if ( menu.classList.contains("hide")){
+        menu.classList.toggle("hide");
+    }
+    else{
+        menu.classList.toggle("hide");
+    }
+}
